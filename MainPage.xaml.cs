38e81865adcd538dfd4d309c303b71754a958f92
Patrik Simic <patrik.simic@gmail.com>
2023-05-16 13:33:41 +0200

    format

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -18,14 +18,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using PSPDFKit.Pdf;
 
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace CieloStackOverflowExample
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
@@ -45,6 +39,7 @@ namespace CieloStackOverflowExample
         private async void SetToolbarItems() 
         {
             var toolbarItems = PdfView.GetToolbarItems();
+
             var idxPan = toolbarItems.IndexOf(new PanToolbarItem());
             if (idxPan >= 0) 
             {
